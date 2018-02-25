efd20ad289f91613bdf9456ebe6b7985bb8a0549
khreez <khreez@gmail.com>
2018-02-25 16:39:34 -0700

    adds blink impl whithout using delay

--- blink/src/main.cpp
+++ blink/src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
 
+int ledPin = 12;
+int ledState = LOW;
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello world!");
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (ledState == LOW)
+      ledState = HIGH;  // Note that this switches the LED *off*
+    else
+      ledState = LOW;   // Note that this switches the LED *on*
+  }
+
+  digitalWrite(ledPin, ledState);
 }
