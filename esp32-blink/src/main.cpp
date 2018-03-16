ff7d300fd834917bb05d25676979df610bf33ff0
khreez <cromero@byrne.local>
2018-03-15 21:12:58 -0700

    adds blink version for esp32

---
+++ esp32-blink/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#ifndef LED_BUILTIN
+  // Set LED_BUILTIN if it is not defined by Arduino framework
+  #define LED_BUILTIN 12
+#endif
+
+int ledState = LOW;
+
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (ledState == LOW)
+      ledState = HIGH;  // Note that this switches the LED *off*
+    else
+      ledState = LOW;   // Note that this switches the LED *on*
+  }
+
+  digitalWrite(LED_BUILTIN, ledState);
+}
